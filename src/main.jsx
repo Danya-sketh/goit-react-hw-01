1d71344d0269bb0d29bf69a7b01c501c7963ab7c
Danya-sketh <yannitc7@gmail.com>
2024-05-04 12:58:15 +0300

    start

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './components/App'
+
+
+function App() {
+  return (
+<div>
+    <p>Hello</p>
+  </div>
+  );
+  
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <App/>
   </React.StrictMode>,
 )
