334135d82b3dc6a05846320416356f42a468b0e7
Danya-sketh <yannitc7@gmail.com>
2024-05-04 14:21:08 +0300

    content

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from '../App'
 
 
-function App() {
-  return (
-<div>
-    <p>Hello</p>
-  </div>
-  );
+// // function App() {
+// //   return (
+// // <div>
+// //     <p>Hello</p>
+// //   </div>
+// //   );
   
-}
+// // }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-   <App/>
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//    <App/>
+//   </React.StrictMode>,
+// )
