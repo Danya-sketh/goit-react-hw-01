ae418fdf7928ee27b697ef928b475f32b08e9369
Danya-sketh <yannitc7@gmail.com>
2024-05-04 19:30:49 +0300

    content 2

---
+++ src/components/App.jsx
@@ -0,0 +1,39 @@
+import Profile from './Profile';
+import Statistics from './Statistics';
+import FriendList from './FriendList';
+import FriendListItem from './FriendListItem';
+import TransactionHistory from './TransactionHistory';
+import Container from './Container';
+import userInfo from './user.json';
+import statisticalData from './statistical-data.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
+
+function App() {
+  return (
+    <Container>
+      <Profile
+        name={userInfo.name}
+        tag={userInfo.tag}
+        location={userInfo.location}
+        avatar={userInfo.avatar}
+        stats={userInfo.stats}
+      />
+      <Statistics stats={statisticalData} />
+      <FriendList>
+        {friends.map(friend => (
+          <FriendListItem
+            key={friend.id}
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+            id={friend.id}
+          />
+        ))}
+      </FriendList>
+      <TransactionHistory items={transactions} />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/App'
+// import Profile from './components/Profile';
+// import Statistics from './components/Statistics';
+// import FriendList from './components/FriendList';
+// import FriendListItem from './components/FriendListItem';
+// import TransactionHistory from './components/TransactionHistory';
+// import Container from './components/Container';
+// import userInfo from './user.json';
+// import statisticalData from './statistical-data.json';
+// import friends from './friends.json';
+// import transactions from './transactions.json';
+
+
+
+// // function App() {
+// //   return (
+// // <div>
+// //     <p>Hello</p>
+// //   </div>
+// //   );
+  
+// // }
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <App/>
+  </React.StrictMode>,
+)
