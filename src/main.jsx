ef7b124b9968aba7ca0e11a6bc88837030351418
Danya-sketh <yannitc7@gmail.com>
2024-05-04 16:42:49 +0300

    fix imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,17 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from '../App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/App'
+// import Profile from './components/Profile';
+// import Statistics from './components/Statistics';
+// import FriendList from './components/FriendList';
+// import FriendListItem from './components/FriendListItem';
+// import TransactionHistory from './components/TransactionHistory';
+// import Container from './components/Container';
+// import userInfo from './user.json';
+// import statisticalData from './statistical-data.json';
+// import friends from './friends.json';
+// import transactions from './transactions.json';
+
 
 
 // // function App() {
@@ -12,8 +23,8 @@
   
 // // }
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//    <App/>
-//   </React.StrictMode>,
-// )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <App/>
+  </React.StrictMode>,
+)
